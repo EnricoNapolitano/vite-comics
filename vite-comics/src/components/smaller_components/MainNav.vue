df65fe3dd9555fd22062d82bcbea8f260f1bd1a9
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-20 21:54:16 +0100

    smaller component: MainNav.vue

---
+++ vite-comics/src/components/smaller_components/MainNav.vue
@@ -0,0 +1,92 @@
+<script>
+export default {
+    name: 'MainNav',
+    data() {
+        return {
+            // data structure of main navbar
+            nav: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+
+        }
+    }
+}
+</script>
+<template>
+    <ul>
+        <li v-for="el in nav" :key="el.text"><a href="#">{{ el.text }}</a></li>
+    </ul>
+</template>
+<style lang="scss" scoped>
+@use '../../assets/scss/partial/mixins.scss' as *;
+@use '../../assets/scss/partial/variables.scss' as *;
+
+ul {
+    @include flex-jsb; //display flex, justify content space between
+}
+
+li {
+    text-transform: uppercase;
+}
+
+a {
+    font-family: "Open Sans Condensed", sans-serif;
+    font-weight: bold;
+    padding: 15px;
+    color: $grey;
+
+    &.active,
+    &:hover {
+        color: $blue;
+    }
+}
+</style>

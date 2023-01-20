471a5bb227038ebceb10dbe89d3a2e0c8c539f36
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-20 22:12:32 +0100

    imported AppMain in App

--- vite-comics/src/App.vue
+++ vite-comics/src/App.vue
@@ -1,15 +1,16 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
   name: 'DC Comics',
-  components: { AppHeader }
+  components: { AppHeader, AppMain }
 }
 </script>
 
 
 <template>
   <app-header></app-header>
-  <main></main>
+  <app-main></app-main>
   <footer></footer>
 </template>
 

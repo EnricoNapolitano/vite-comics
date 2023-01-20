f8588bac45caebb6f8bda2094e2efbbd920b051c
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-20 22:11:56 +0100

    AppMain

---
+++ vite-comics/src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script></script>
+<template>
+    <!-- Here will start the main -->
+    <div>
+        <!-- tag placeholder -->
+        <h1 class="container">-- Contents go here --</h1>
+    </div>
+</template>
+<style lang="scss" scoped>
+@use '../assets/scss/partial/variables.scss' as *;
+
+div {
+    height: 130px;
+    background-color: $black; //color from variablse.scss
+    color: white;
+}
+
+//tag place holder
+h1 {
+    line-height: 130px;
+}
+</style>

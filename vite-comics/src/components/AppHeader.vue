ac0403e8597bf20f2c69ccaf207d303d65b35ce2
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-20 17:12:20 +0100

    AppHeader

---
+++ vite-comics/src/components/AppHeader.vue
@@ -0,0 +1,94 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            nav: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+
+        }
+    }
+}
+</script>
+
+<template>
+    <header class="container">
+        <nav>
+            <figure><img src="../assets/img/dc-logo.png" alt="logo DC"></figure>
+            <ul>
+                <li v-for="el in nav" :key="el.text"><a href="#">{{ el.text }}</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partial/mixins.scss' as *;
+
+nav {
+    height: 120px;
+    @include flex-jsb-ac()
+}
+
+ul {
+    @include flex-jsb
+}
+
+li {
+    text-transform: uppercase;
+}
+
+a {
+    padding: 15px;
+}
+</style>

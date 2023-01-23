860f49e60187cef575b8fd1aeb9acf4362ac95e4
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-23 18:39:41 +0100

    created component "ComicCard"

---
+++ vite-comics/src/components/smaller_components/ComicCard.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'ComicCard',
+    props: {
+        comic: Object //imported single comic element from AppMain
+    }
+}
+</script>
+
+<template>
+    <!-- Single comic: this is the component that will be repeated -->
+    <li>
+        <div>
+            <figure>
+                <a href="#"><img :src="comic.thumb" :alt="comic.series"></a>
+            </figure>
+            <h5>{{ comic.series }}</h5>
+        </div>
+    </li>
+</template>
+
+<style lang="scss" scoped>
+// specifi style to create the 'comic card'
+figure {
+    width: 185px;
+    height: 185px;
+    overflow: hidden;
+}
+
+h5 {
+    text-transform: uppercase;
+    font-weight: 500;
+    width: 90%;
+    padding: 20px 0 30px 0;
+}
+</style>

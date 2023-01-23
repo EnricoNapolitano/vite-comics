04fd8fafa82be3e302adbbc315e200670e8ded5b
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-23 22:33:32 +0100

    created prop 'nav' for the central navbar

--- vite-comics/src/App.vue
+++ vite-comics/src/App.vue
@@ -95,6 +95,39 @@ export default {
           type: 'graphic novel',
         },
       ],
+      //central navbar data structure (the one with blue background)
+      centralNav: [
+        {
+          text: 'Digital Comics',
+          img: 'buy-comics-digital-comics.png',
+          url: '#',
+          current: false,
+        },
+        {
+          text: 'DC Merchandise',
+          img: 'buy-comics-merchandise.png',
+          url: '#',
+          current: true,
+        },
+        {
+          text: 'subscription',
+          img: 'buy-comics-subscriptions.png',
+          url: '#',
+          current: false,
+        },
+        {
+          text: 'Comic shop locator',
+          img: 'buy-comics-shop-locator.png',
+          url: '#',
+          current: false,
+        },
+        {
+          text: 'DC power visa',
+          img: 'buy-dc-power-visa.svg',
+          url: '#',
+          current: false,
+        },
+      ],
     }
   },
   components: { AppHeader, AppJumbotron, AppMain, CentralNav, AppFooter }
@@ -107,7 +140,7 @@ export default {
   <app-jumbotron></app-jumbotron>
   <!-- Passing comics data by prop to AppMain thanks to the attribute "comics" -->
   <app-main :comics="comics"></app-main>
-  <central-nav></central-nav>
+  <central-nav :nav="centralNav"></central-nav>
   <app-footer></app-footer>
 </template>
 
--- vite-comics/src/components/CentralNav.vue
+++ vite-comics/src/components/CentralNav.vue
@@ -1,43 +1,7 @@
 <script>
 export default {
     name: 'CentralNav',
-    data() {
-        return {
-            //navbar data structure
-            nav: [
-                {
-                    text: 'Digital Comics',
-                    img: 'buy-comics-digital-comics.png',
-                    url: '#',
-                    current: false,
-                },
-                {
-                    text: 'DC Merchandise',
-                    img: 'buy-comics-merchandise.png',
-                    url: '#',
-                    current: true,
-                },
-                {
-                    text: 'subscription',
-                    img: 'buy-comics-subscriptions.png',
-                    url: '#',
-                    current: false,
-                },
-                {
-                    text: 'Comic shop locator',
-                    img: 'buy-comics-shop-locator.png',
-                    url: '#',
-                    current: false,
-                },
-                {
-                    text: 'DC power visa',
-                    img: 'buy-dc-power-visa.svg',
-                    url: '#',
-                    current: false,
-                },
-            ],
-        }
-    },
+    props: { nav: Array },
     methods: {
         //method to create path image
         buildImagePath(image) {

3bff0166395ba42d45b2852a031b73a935414b8c
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-23 21:52:28 +0100

    jumbotron

---
+++ vite-comics/src/components/AppJumbotron.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'AppJumbotron',
+    data() {
+        return {
+            jumbotron: 'jumbotron.jpg',
+        }
+    },
+    methods: {
+        //method to create jumbotron's path
+        buildJumboPath(img) {
+            return new URL(`../assets/img/${img}`, import.meta.url).href;
+        }
+    }
+}
+</script>
+<template>
+    <figure>
+        <img :src="buildJumboPath(jumbotron)" alt="main banner picture">
+    </figure>
+</template>
+<style lang="scss" scoped>
+figure {
+    height: 400px;
+    overflow: hidden;
+}
+
+img {
+    width: 100%;
+}
+</style>

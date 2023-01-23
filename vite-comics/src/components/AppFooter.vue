28767b03c9600acaf0cb7f2c99f15f8ea5345937
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-23 01:50:16 +0100

    footer: made of 2 components

--- vite-comics/src/components/AppFooter.vue
+++ vite-comics/src/components/AppFooter.vue
@@ -1,11 +1,14 @@
 <script>
+import UpperFooter from './footer_components/UpperFooter.vue'
 import DownerFooter from './footer_components/DownerFooter.vue';
 export default {
     name: 'AppFooter',
-    components: { DownerFooter }
+    components: { UpperFooter, DownerFooter }
 }
 </script>
 <template>
+    <!-- footer made of 2 components -->
+    <upper-footer></upper-footer>
     <downer-footer></downer-footer>
 </template>
 <style>
